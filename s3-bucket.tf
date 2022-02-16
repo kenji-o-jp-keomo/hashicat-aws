116a9a96b7cfa99bf63370e604d7128f2cb8b490
kenji-o-jp-keomo <99773405+kenji-o-jp-keomo@users.noreply.github.com>
2022-02-16 16:41:25 +0900

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/kenji-training/s3-bucket/aws"
+  version = "2.14.1"
+  # insert required variables here
 }
